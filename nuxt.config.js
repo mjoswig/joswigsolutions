41b379d8b71015e6741deb2bb5bfc1688e6f3ae8
Manuel Joswig <manuel@joswigsolutions.com>
2023-07-24 01:35:33 +0200

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/google-gtag',
+    '@nuxtjs/sitemap'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
