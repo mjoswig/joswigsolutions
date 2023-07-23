e03a4abd06e3ed43a346341f613f904c2a9bdf02
Manuel Joswig <manuel@joswigsolutions.com>
2023-07-23 16:19:29 +0200

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Joswig Solutions',
+    title: 'Joswig Solutions - Web Development Company',
     htmlAttrs: {
       lang: 'en'
     },
