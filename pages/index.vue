795fad97acf4e2da22fe1297152e0508f7961c8a
Manuel Joswig <manuel@joswigsolutions.com>
2023-10-28 13:14:34 +0800

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,9 @@
     <div class="text-center mb-6 md:mb-10">
       <h1 class="text-2xl xl:text-3xl mb-2">About Joswig Solutions</h1>
       <p class="text-lg xl:text-xl mb-4 xl:mb-6">We are a web development company with a focus on client services and web solutions.</p>
-      <div class="flex justify-center text-sm xl:text-base">
+      <div class="flex flex-col space-y-2 md:flex-row items-center justify-center md:space-x-4 md:space-y-0 text-sm xl:text-base">
         <Btn @click="openCalendly">Launch Your MVP in 30 Days for $10K</Btn>
+        <nuxt-link to="/nuxt-boilerplate">Pre-order Boilerplate for $124.50</nuxt-link>
       </div>
     </div>
     <div class="mb-8 md:mb-12">
