78a43f0508d5fc850575bfee8511d97dddd749cc
Manuel Joswig <manuel@joswigsolutions.com>
2023-10-28 13:07:31 +0800

    Update

--- pages/nuxt-boilerplate.vue
+++ pages/nuxt-boilerplate.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <div class="flex flex-col space-y-4 md:flex-row md:space-x-8 md:space-y-0 items-center">
-      <div class="w-full">
+      <div class="flex flex-col items-center justify-center text-center md:block md:text-left w-full">
         <h1 class="text-2xl xl:text-3xl mb-2">Nuxt SaaS Boilerplate & Starter Kit – Launch your web app in days, not weeks</h1>
-        <p class="text-lg mb-4">From zero to SaaS with no effort: This Nuxt 2 boilerplate comes with all the tools you need to build your next game-changing web app.</p>
-        <Btn @click="preOrder">Pre-order for $124.50</Btn>
+        <p class="text-lg mb-2">From zero to SaaS with no effort: This Nuxt 2 boilerplate comes with all the tools you need to build your next game-changing web app.</p>
+        <p class="text-lg mb-4">Pre-order today and follow me <a href="https://twitter.com/manuel_joswig">@manuel_joswig</a> for updates. You can ask for a refund anytime.</p>
+        <Btn class="w-fit" @click="preOrder">
+          <span>Pre-order for <strike>$249.00</strike> $124.50</span>
+        </Btn>
       </div>
       <div class="w-full">
         <img src="@/assets/images/nuxt-boilerplate.svg" />
