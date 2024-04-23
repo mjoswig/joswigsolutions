5854181a621249839e0590e0696994bf8ab74b76
Manuel Joswig <manuel@joswigsolutions.com>
2024-04-23 02:40:04 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
         />
         <Card
           title="Digital Product Design"
-          description="We build the foundation for a high-end digital product with smooth interactions and an impeccable look & feel."
+          description="We craft high-end digital products with an impeccable look & feel that your customers and employees want and need."
           icon="services/digital-product-design.svg"
         />
         <Card
