22b606b4de1600fd20db78c560467064f89adcea
Manuel Joswig <manuel@joswigsolutions.com>
2024-05-13 11:33:34 +0200

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
         />
         <Card
           title="Traumanwalt"
-          description="A directory that connects German lawyers with clients looking for legal advice."
+          description="A web directory to find the best lawyers and law firms based in Germany."
           label="2023"
           icon="solutions/traumanwalt.png"
           icon-width="64px"
